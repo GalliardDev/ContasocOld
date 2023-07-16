3b6ec6c135ba31f3d736a991b9d3c38b6b239db0
yoshibv <jomaamga@outlook.es>
2023-07-16 07:08:07 +0200

    Minor bugfix. Arreglada lista de espera impresa. Contasoc v5.0.1.

--- src/es/yoshibv/contasoc/Main.java
+++ src/es/yoshibv/contasoc/Main.java
@@ -17,7 +17,7 @@ import es.yoshibv.contasoc.gui.NewMainWindow;
 import es.yoshibv.contasoc.util.ErrorHandler;
 
 public class Main {
-	public static final String VERSION_APP = "Contasoc v5.0.0";
+	public static final String VERSION_APP = "Contasoc v5.0.1";
 	public static final String ESCRITORIO = "C:/Users/" + System.getenv("USERNAME") + "/Desktop";
 	public static final String BDD = "C:/Contasoc/contasoc.db";
 	
